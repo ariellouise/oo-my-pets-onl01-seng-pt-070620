7c0af0333efcf67ab8a21192c1544db35aa18ffc
ariel murphy <arielladders@gmail.com>
2020-08-04 01:26:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
-  attr_reader :name, :species 
+  attr_reader :name, :species #getter for grabbing the animals 
+  @@all = []
+  
+  def initialize(name) #call species
+    @name = name 
+    @species = "human"
+    @@all << self 
+  end 
+  
+  def say_species
+    "I am a #{self.species}."
+  end 
+  
+  def self.all #define self 
+    @@all 
+  end 
+  
+  
 end
