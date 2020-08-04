963ff8c27dc9de51baa8d19b1236e64eb38ed7ad
ariel murphy <arielladders@gmail.com>
2020-08-04 01:23:46 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,17 @@
 class Cat
-  attr_accessor :owner, :mood
-  def initialize(name)
+  attr_accessor :owner, :mood #call on both
+  attr_reader :name #getter 'get the name'
+  
+  @@all = [] #class variable shared between classes
+  
+  def initialize(name, owner)
     @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @@all << self
   end 
   
+  def self.all #have to define self
+    @@all 
+  end 
 end
