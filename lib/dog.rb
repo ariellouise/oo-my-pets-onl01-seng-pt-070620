4b80070ba4410471a3153d56740b78002dc1ee88
ariel murphy <arielladders@gmail.com>
2020-08-04 01:21:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
 class Dog
   attr_accessor :owner, :mood #call on both
-  attr_reader :name #instance
+  attr_reader :name #just recognize it 
   
-  @@all = []
+  @@all = [] #class variable shared between classes
   
   def initialize(name, owner)
     @name = name 
+    @owner = owner 
+    @mood
   end 
   
 end
